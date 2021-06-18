7d9007f553a2c09570bff1e16841326561e486f9
JuanManuel2421 <juanmanuelrs2504mail.com>
2021-06-17 19:05:01 -0500

    Archivos de la tarea #6

---
+++ Archivos_tarea_6/hola.c
@@ -0,0 +1,18 @@
+/*Prueba de integridad*/
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+	int a = 61;
+	int b = 13;
+	int c = 0;
+
+	printf("Prueba de integridad");
+	c = a+b;
+	printf (c);
+
+	return 0;
+}
